b850f44def6997b057d02d472dbf84243bab52e7
Kyle Nazario <kyle@servicecore.com>
2020-05-07 20:55:53 -0600

    Tightens up build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './dist/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.min.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
