941c1c8a409efa81760eef3cd545ea83d3fa6828
Kyle Nazario <kyle@servicecore.com>
2020-05-08 11:49:27 -0600

    Adds separate prod build pipeline

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './dist/index.js',
+  output: {
+    filename: 'index.min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'production',
+}
