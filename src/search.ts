86f27afc16a0fb7043615de7c4da17720426cbb6
Kyle Nazario <kyle@servicecore.com>
2020-05-09 15:01:39 -0600

    Browser support for search

--- src/search.ts
+++ src/search.ts
@@ -1,6 +1,19 @@
 import fetch from 'isomorphic-fetch';
 import {JSDOM} from 'jsdom';
 import AmazonSearchResult from './amazon-search-result';
+// @ts-ignore
+import {isBrowser} from 'browser-or-node';
+
+interface AllOriginsResponse {
+  contents: string; // html string
+  status: {
+    content_length: null;
+    content_type: string;
+    http_code: null;
+    response_time: number;
+    url: string;
+  }
+}
 
 /**
  * @name extractResults
@@ -16,6 +29,15 @@ function extractResults(virtualDom: JSDOM): AmazonSearchResult[] {
   });
 }
 
+function queryToRequest(query: string): string {
+  return `https://www.amazon.com/s?k=${encodeURIComponent(query)}&ref=nb_sb_noss`;
+}
+
+function queryToProxiedRequest(query: string): string {
+  let url = queryToRequest(query);
+  return 'http://api.allorigins.win/get?url=' + encodeURIComponent(url);
+}
+
 /**
  * @name searchAmazon
  * @description Scrapes the first page of Amazon search results
@@ -25,9 +47,16 @@ function extractResults(virtualDom: JSDOM): AmazonSearchResult[] {
  * @returns {Promise<Array.<AmazonSearchResult>>}
  */
 export default async function searchAmazon(query: string, includeSponsoredResults?: boolean): Promise<AmazonSearchResult[]> {
-  const resp = await fetch(`https://www.amazon.com/s?k=${encodeURIComponent(query)}&ref=nb_sb_noss`);
-  const pageText = await resp.text();
-  let searchResults = extractResults(new JSDOM(pageText));
+  let pageHtml: string;
+  if (isBrowser) {
+    const resp = await fetch(queryToProxiedRequest(query));
+    const body: AllOriginsResponse = await resp.json();
+    pageHtml = body.contents;
+  } else {
+    const resp: Response = await fetch(queryToRequest(query));
+    pageHtml = await resp.text();
+  }
+  let searchResults = extractResults(new JSDOM(pageHtml))
   if (!includeSponsoredResults) {
     searchResults = searchResults.filter(result => !result.sponsored);
   }
