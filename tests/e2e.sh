51534e25c68549134bc275a417c53672da232db2
Kyle Nazario <knazario@bitovi.com>
2021-05-05 14:17:24 -0600

    Simplifies cleanup

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -9,14 +9,12 @@ npm link unofficial-amazon-search
 touch server.js
 printf "const {searchAmazon} = require('unofficial-amazon-search');\nsearchAmazon('lamp').then(data => console.log(data.searchResults.length > 0));" > server.js
 node server.js | grep 'true' &> /dev/null
+cd ..
+rm -rf tester
 if [ $? == 0 ];
 then
-  cd ..
-  rm -rf tester
   echo "Passed"
 else
-  cd ..
-  rm -rf tester
   echo "No results returned"
   exit 1
 fi
