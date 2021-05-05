461a2e4a05c8853f269713038808da254b2bc357
Kyle Nazario <knazario@bitovi.com>
2021-05-05 14:07:57 -0600

    Cleans up test script

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,6 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
 npm link
+rm -rf tester
 mkdir tester
 cd tester
 npm init -y
@@ -8,12 +9,14 @@ npm link unofficial-amazon-search
 touch server.js
 printf "const {searchAmazon} = require('unofficial-amazon-search');\nsearchAmazon('lamp').then(data => console.log(data.searchResults.length > 0));" > server.js
 node server.js | grep 'true' &> /dev/null
-cd ..
-rm -rf tester
 if [ $? == 0 ];
 then
+  cd ..
+  rm -rf tester
   echo "Passed"
 else
+  cd ..
+  rm -rf tester
   echo "No results returned"
   exit 1
 fi
